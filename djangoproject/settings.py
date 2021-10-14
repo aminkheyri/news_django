c49a2a8875afc338431f4dfa1e3804cd49066908
amin <aminkheyrri@gmail.com>
2021-10-14 20:09:09 +0330

    CELERY CONFIG

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -126,5 +126,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #CELERY
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
